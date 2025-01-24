ce8b26bcc9549adb90f606afce2959f5341d0dde
walid-hud <walidthehud@gmail.com>
2025-01-24 03:06:33 +0100

    finished , but it still has no security

--- App.py
+++ App.py
@@ -1,33 +1,36 @@
-from flask import Flask , request , make_response
+from flask import Flask, request, make_response, jsonify, send_file
+import os
+from io import BytesIO
 from rembg import remove
+from PIL import Image
 
 app = Flask(__name__)
 
-@app.route('/' , methods=['POST'])
-def removeBG():
-    if request.method  != 'POST':
-        response = make_response()
-        response.status = 409
-        return response
+@app.route('/', methods=['POST'])
+def main():
+    try:
+        if 'file' not in request.files:
+            return 'test'
+        file = request.files['file']
+        if file.filename == '':
+            return make_response(jsonify({"error": "no file sent"}), 400)
+        if file.mimetype not in ['image/jpeg', 'image/jpg', 'image/png']:
+            return make_response(jsonify({"error": "unsupported image format"}), 400)
+        
+        original_img = Image.open(file)
+        edited_img = remove(original_img)
+        
+        img_io = BytesIO()
+        edited_img.save(img_io, format='PNG')
+        img_io.seek(0)
+        
+        original_img.close()
+        edited_img.close()
+        
+        return send_file(img_io, mimetype='image/png')
+    except Exception as e:
+        print(e)
+        return make_response(jsonify({"error": "internal server error"}), 500)
 
- 
-    if request.files == '' :
-        response = make_response()
-        response.status = 500
-        return response
-
-    file = request.files['file']
-    if file.filename == ' ':
-        response = make_response()
-        response.status = 500
-        return response
-    
-    if file.mimetype != 'jpeg' or 'jpg' or 'png':
-        response = make_response()
-        response.status = 500
-        return response
-    
-    
-if  __name__ == '__main__' :
-    app.run('localhost' , 8080 , True)
-
+if __name__ == '__main__':
+    app.run('localhost', 8080, debug=False)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-a simple flask app that removes images backgrounds using the rembg library 😐
+a simple <a href="https://github.com/pallets/flask">Flask</a> app that removes the background from images using the <a href="https://github.com/danielgatis/rembg"> rembg</a> library
+<br>
+The app is served using  <a href="https://github.com/Pylons/waitress"> Waitress</a>
+
+<p style="opacity:0.6; font-size:0.4rem;">I have no idea what I'm doing lol<p>
